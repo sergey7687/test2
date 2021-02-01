e4196d7e526c27fd55625c00b6c4816bdce4bcf1
Sergey Naumon <sergey.naumov@myrambler.ru>
2021-02-01 15:24:55 +0300

    added func

--- examle.py
+++ examle.py
@@ -1,4 +1,3 @@
-# easy unpack
 def easy_unpack(elements):
     """
         returns a tuple with 3 elements - first, third and second to the last
@@ -9,7 +8,6 @@ def easy_unpack(elements):
 print(easy_unpack((1, 2, 3, 4, 5, 6, 7, 9)))
 
 
-# first word
 def first_word(text: str) -> str:
     """
         returns the first word in a given text.
@@ -19,13 +17,12 @@ def first_word(text: str) -> str:
 
 print(first_word('hello world'))
 
-# password
+
 def is_acceptable_password(password: str) -> bool:
     # your code here
     return len(password) > 6
 
 
-# end zeroes
 def end_zeros(n):
     return len(str(n)) - len(str(n).rstrip('0'))
 
@@ -33,20 +30,20 @@ def end_zeros(n):
 print(end_zeros(1124234000))
 
 
-# backwordstring
 def backward_string(val: str) -> str:
     return val[::-1]
 
 
 print(backward_string('hellooo'))
 
-# remove all before
+
 def remove_all_before(list_remove, val):
     if val not in list_remove:
         return list_remove
     else:
         r_f = list_remove.index(val)
         return list_remove[r_f:]
+
 
 print(remove_all_before([1, 2, 3, 4, 5], 3))
 
@@ -55,4 +52,26 @@ def is_all_upper(s):
     newlist = [i.isupper() for i in s.split() if not i.isdigit()]
     return all(newlist)
 
+
 print(is_all_upper('ALL 112'))
+
+
+def number_length(a: int) -> int:
+    return len(str(a))
+
+
+def replace_first(par):
+    item = par.pop(0)
+    par.append(item)
+    return par
+    # return items[1:] + items[:1]
+
+
+print(replace_first([1, 2, 3, 4]))
+
+
+def max_digit(par):
+    return int(max(str(par)))
+
+
+print(max_digit(696))
