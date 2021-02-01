6a6407bfa20fcf0c802bee4a8d72fce21397faad
Sergey Naumon <sergey.naumov@myrambler.ru>
2021-02-01 14:04:40 +0300

    added func

--- examle.py
+++ examle.py
@@ -1,31 +1,58 @@
-class First:
-    def __init__(self, a, b):
-        self.a = a
-        self.b = b
-
-    def disp(self):
-        return self.a, self.b
-
-    def new(self):
-        return self.a.upper()
-
-    def __repr__(self):
-        return f'names: {self.a} {self.b}'
+# easy unpack
+def easy_unpack(elements):
+    """
+        returns a tuple with 3 elements - first, third and second to the last
+    """
+    return tuple(elements[i] for i in [0, 2, -2])
 
 
-f = First('serg', 'naumov')
-print(f)
+print(easy_unpack((1, 2, 3, 4, 5, 6, 7, 9)))
 
 
-class Second(First):
-    def __init__(self, a, b, c=None):
-        super().__init__(a, b)
-        self.c = c
+# first word
+def first_word(text: str) -> str:
+    """
+        returns the first word in a given text.
+    """
+    return text.split()[0]
 
 
+print(first_word('hello world'))
+
+# password
+def is_acceptable_password(password: str) -> bool:
+    # your code here
+    return len(password) > 6
 
 
-s = Second('vasili', 'slinin')
-print(s)
-s.new()
-print(s.a)
+# end zeroes
+def end_zeros(n):
+    return len(str(n)) - len(str(n).rstrip('0'))
+
+
+print(end_zeros(1124234000))
+
+
+# backwordstring
+def backward_string(val: str) -> str:
+    return val[::-1]
+
+
+print(backward_string('hellooo'))
+
+# remove all before
+def remove_all_before(list_remove, val):
+    if val not in list_remove:
+        return list_remove
+    else:
+        r_f = list_remove.index(val)
+        return list_remove[r_f:]
+
+print(remove_all_before([1, 2, 3, 4, 5], 3))
+
+
+def is_all_upper(s):
+    newlist = [i.isupper() for i in s.split() if not i.isdigit()]
+    return all(newlist)
+
+print(is_all_upper('ALL 112'))
